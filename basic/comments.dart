e8049dc4bddf9b4429d910db7ba575258d076091
chiagoziemng <chiagoziemjamesng@gmail.com>
2023-06-28 20:01:54 +0100

    first commit

---
+++ basic/comments.dart
@@ -0,0 +1,62 @@
+// This is a single line comment
+
+
+// int main()
+// {
+//   double area = 3.14 * 4 * 4;
+//   // It prints the area
+//   // of a circle of radius = 4
+//   print(area);
+
+//   return 0;
+// }
+
+
+
+/* 
+
+These are 
+
+multiple line
+
+of comments
+
+*/
+
+
+// int main()
+// {
+//   var lst = [1, 2, 3];
+
+//   /* 
+//   it prints
+//   the whole list
+//   at once
+//   */
+//   print(lst);
+
+//   return 0;
+// }
+
+
+
+bool checkEven(n){
+  /// Return true
+  /// if n is even
+  if(n%2==0)
+
+    return true;
+
+    ///
+    ///Return fase if n is odd
+  else
+
+  return false; 
+}
+
+int main()
+{
+  int n = 43;
+  print(checkEven(n));
+  return 0;
+}
---
+++ datatype/datatypes.dart
@@ -0,0 +1,19 @@
+// data types are :
+// Number, Strings, Booleans, Lists, Sets, Maps, 
+//
+// Other content here
+//Other types, Type inference, Public/private variables, Null safety, LAte variables, 
+
+num a = 3;
+
+// OR 
+
+num b = 3.14;
+
+
+
+// use this to run code
+main() {
+  print(b);
+}
+
